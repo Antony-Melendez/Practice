6fe4529c491a1dcbc7c14947c87acee03440530c
Antony Melendez <50185974+Antony-Melendez@users.noreply.github.com>
2020-09-25 12:30:20 -0700

    New

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Programmer: Antony Melendez
+Description: juest testing 
+Date: 09/24/20
+*/
+using System;
+namespace School
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the following info: ");
+            Console.WriteLine("Total items in menu: ");
+            int mItems = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Total drink: ");
+            int dItems = Convert.ToInt32(Console.ReadLine());
+
+            float tip = 0.15f;
+            float mPrice = 1.75f;
+            float dPrice = 1f;
+
+            float mTotal = mItems * mPrice;
+            float dTotal = dItems * dPrice;
+
+
+            float aTotal = mTotal + dTotal * tip;
+
+            Console.WriteLine("Your total is: $" + aTotal);
+        }
+    }
+}
