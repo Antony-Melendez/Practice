38dff5ca35468ac89a89e1b4bd8bf308691fd580
Antony Melendez <50185974+xpredatorx338@users.noreply.github.com>
2020-10-21 15:17:55 -0700

    added c sharp environment

---
+++ C#/IntroLibrary/personModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IntroLibrary
+{
+    public class personModel
+    {
+        public static string FirstName;
+        public static string LastName;
+        public static int Age;
+    }
+}
---
+++ C#/IntroUI/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using IntroLibrary;
+
+namespace IntroUI
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            greetUser();
+            input();
+        }
+
+        private static void greetUser()
+        {
+            Console.WriteLine("Welcom!");
+            Console.WriteLine("Please enter the following inputs: ");
+        }
+
+        static void input()
+        {
+            Console.WriteLine("Enter you first name: ");
+            personModel.FirstName = Console.ReadLine();
+            Console.WriteLine("Enter you last name: ");
+            personModel.LastName = Console.ReadLine();
+            Console.WriteLine("Enter you age: ");
+            personModel.Age = Convert.ToInt32(Console.ReadLine());
+
+            exitProgram();
+        }
+
+        private static void exitProgram()
+        {
+            Console.WriteLine("Goodbye!");
+        }
+    }
+}
