aa7fd034fb346e3c542df91e57959c392f10d5ec
Antony Melendez <50185974+xpredatorx338@users.noreply.github.com>
2020-11-17 15:51:11 -0800

    w

---
+++ Python/ToBeGraded/FinalProject/bankapp.py
@@ -0,0 +1,8 @@
+''''
+Programmer: Antony Melendez
+
+Description: this code is to emulate a bank app by being able to log in new user or existing users and being able to deposit mondey or withdraw money from existing accounts
+
+Date: 11-17-2020
+
+'''
