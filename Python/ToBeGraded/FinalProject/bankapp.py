b75736d02669f79afadc03dde87557d7523dc488
Antony Melendez <50185974+xpredatorx338@users.noreply.github.com>
2020-11-19 14:07:49 -0800

    ugh

--- Python/ToBeGraded/FinalProject/bankapp.py
+++ Python/ToBeGraded/FinalProject/bankapp.py
@@ -1,8 +1,43 @@
 ''''
 Programmer: Antony Melendez
 
-Description: this code is to emulate a bank app by being able to log in new user or existing users and being able to deposit mondey or withdraw money from existing accounts
+Description: this code is to emulate a bank app by being able to log in new user or existing users
+and being able to deposit mondey or withdraw money from existing accounts
 
 Date: 11-17-2020
 
 '''
+
+f_read = open("UserInformtion.txt", 'r')
+
+Usernames = []
+passWords = []
+balences = []
+
+for i in f_read:
+    infoList = i.split()
+    Usernames.append(infoList[0])
+    passWords.append(infoList[1])
+    balences.append(infoList[2])
+
+    print(Usernames)
+    print(passWords)
+    print(balences)
+
+
+def userVerification():
+    userNameInput = input("Enter your username: ")
+    if userNameInput in Usernames:
+        userpasswordInput = input(userNameInput + " Enter your password: ")
+
+    if userpasswordInput in passWords:
+        LoggedIn(userNameInput)
+    else:
+        print("Wrong Information try again: ")
+
+
+def LoggedIn(userNameInput):
+    print("Welcome! " + userNameInput)
+
+
+userVerification()
--- Python/test1.py
+++
@@ -1,7 +0,0 @@
-firtName = "Antony"
-# print(firtName[3])
-
-
-print("the lengh : ", len(firtName))
-for i in range(len(firtName)):
-    print(firtName[i])
