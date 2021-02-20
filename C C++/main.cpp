cdbc11abf7463a5f91dd65508b108d53aff4d142
Antony Melendez <50185974+xpredatorx338@users.noreply.github.com>
2021-02-20 15:42:56 -0800

    w

--- C C++/main.cpp
+++ C C++/main.cpp
@@ -1,12 +1,45 @@
 #include <iostream>
-#include <cmath>
-
 using namespace std;
 
-int main() {
-	
-    
-   
-   
+string getDayOfTheWeek(int dayNum)
+{
+    string dayName;
+
+    switch (dayNum)
+    {
+    case 0:
+        dayName = "Sunday";
+        break;
+    case 1:
+        dayName = "Monday";
+        break;
+    case 2:
+        dayName = "Tuesday";
+        break;
+    case 3:
+        dayName = "Wednesday";
+        break;
+    case 4:
+        dayName = "Thrusday";
+        break;
+    case 5:
+        dayName = "Friday";
+        break;
+    case 6:
+        dayName = "Saturday";
+        break;
+
+    default:
+        dayName = "N/A";
+        break;
+    }
+
+    return dayName;
+}
+
+int main()
+{
+
+    cout << getDayOfTheWeek(10);
     return 0;
 }
---
+++ C C++/tempCodeRunnerFile.cpp
@@ -0,0 +1,2 @@
+
+        dayName = "Saturday"
